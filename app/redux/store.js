5b62de99d0acae3f386ecb8e19526d999c7d38e6
Freelanceromarr <freelanceromarr@gmail.com>
2023-10-16 02:30:56 +0600

    flight delete system done by updating redux state

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,8 +1,19 @@
 
-import { createStore }  from "redux"
+import { createStore, applyMiddleware }  from "redux"
 import bookingReducer from "./booking/bookingReducer"
 
+// middlewate to limit flight Listing @using curried function
 
-const store = createStore(bookingReducer)
+const limitListing = (store) =>(next)=>(action)=>{
+    const stateCurrentValue = store.getState()
+    if (stateCurrentValue && stateCurrentValue.value.length >2 ) {
+        console.log("more than 3 listing ");
+    }else{
+        next(action)
+    }
+
+}
+
+const store = createStore(bookingReducer, applyMiddleware(limitListing))
 
 export default store ;
